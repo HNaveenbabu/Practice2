a4932be1e6873b360dc2301cb25e79bf5fea71e9
HNaveenbabu <hasanapuram1299@gmail.com>
2022-12-20 06:44:26 +0530

    Create FirstCode.java

---
+++ MyFirstJavaProject/src/com/project/new1/FirstCode.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.project.new1;
+
+/**
+ * @author hasan
+ *
+ */
+public class FirstCode {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
